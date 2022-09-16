8bc55465569c2b4ce78d86d655855b62f80825ad
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-09-16 17:49:09 -0500

    created props for messenger

--- client/src/pages/Messenger.js
+++ client/src/pages/Messenger.js
@@ -1,15 +1,24 @@
 // React 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MessagesBox from '../components/MessagesBox'
 import MessagingForm from '../components/MessagingForm'
 
 // Styles
+import '../styles/pages/Messenger.css'
 
 export default function Messenger() {
+  // set variables
+  const [MessagesData, setMessagesData] = useState([]);
+
+  const handleFormSubmit = (event) => {
+    const { name, value } = event.target;
+    setMessagesData({ ...MessagesData, [name]: value });
+  };
+
   return (
     <div>
-      <MessagesBox />
-      <MessagingForm />
+      <MessagesBox messagesData={MessagesData} />
+      <MessagingForm onSubmit={handleFormSubmit}/>
     </div>
   );
 }
