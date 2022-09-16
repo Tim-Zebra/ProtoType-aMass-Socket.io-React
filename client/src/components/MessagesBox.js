7ac68556891dbca80986eb4549b9206678c10642
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-09-16 18:02:33 -0500

    base code for messages added

--- client/src/components/MessagesBox.js
+++ client/src/components/MessagesBox.js
@@ -4,15 +4,24 @@ import React, { useState, useEffect } from 'react';
 // Styles
 import '../styles/components/MessagesBox.css'
 
-export default function MessagesBox() {
-  // set variables
-  const [MessagesData, setMessagesData] = useState({});
-  
-  // get local storage if exists
+export default function MessagesBox(messagesData) {
+  // functions
+  const printAllMessages = (messages) => {
+    for(let k = 0; k < messages.length; k++) {
+      let message = messages[k];
+      const printMessage = (message) => {
+        return (
+          <li>
+            {message}
+          </li>
+        )
+      }
+    }
+  };
   return (
     <div>
       <ul id="messages">
-        
+        {printAllMessages(messagesData)}
       </ul>
     </div>
   );
